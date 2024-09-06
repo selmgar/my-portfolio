6a2f370d7dea9fd3e2da23cf7241514bdf0640e6
@selmgar-eEHSZr <selmgar@gmail.com>
2024-09-06 16:12:57 -0500

    Fixes a bracing issue in the resume.css which was breaking my production css

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -6,18 +6,16 @@ function Nav() {
   return (
     <nav>
       <ul className="nav-list"> 
-        {/* The NavLink component is used to create a link to a different route.
-            By default, an active class is added to a <NavLink> component when it is active so you can use CSS to style it. */}
-        <li>
+        <li className="list-item">
           <NavLink to="/">About me</NavLink>
         </li>
-        <li>
+        <li className="list-item">
           <NavLink to="/portfolio">Portfolio</NavLink>
         </li>
-        <li>
+        <li className="list-item">
           <NavLink to="/contact">Contact me</NavLink>
         </li>
-        <li>
+        <li className="list-item">
           <NavLink to= "/resume">Resume</NavLink>
         </li>
       </ul>
