eb9321390e195d839d38785ef9a590ec2f6aa041
@selmgar-eEHSZr <selmgar@gmail.com>
2024-12-10 12:02:15 -0600

    Adds coding quiz gif

--- src/assets/imageMap.js
+++ src/assets/imageMap.js
@@ -2,12 +2,14 @@ import bananaflix from './bananaflix.gif';
 import bookshelf from './bookshelf.gif';
 import me from './me.jpg';
 import weatherdashboard from './weatherdashboard.gif';
+import codingquiz from './coding-quiz.gif';
 
 const imageMap = {
   'me.jpg': me,
   'bananaflix.gif': bananaflix,
   'bookshelf.gif': bookshelf,
   'weatherdashboard.gif': weatherdashboard,
+  'coding-quiz.gif': codingquiz,
 };
 
 export default imageMap;
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -11,6 +11,7 @@ function Portfolio () {
       <section className="projects">
         <Project imageName="bananaflix.gif" title="BananaFlix" link="https://bananaflix-fdyk.onrender.com/" githubRepo="https://github.com/selmgar/bananaflix" />
         <Project imageName="bookshelf.gif" title="Bookshelf" link="https://selmgar.github.io/my-book-list/my-book-list.html" githubRepo="https://github.com/selmgar/my-book-list" />
+        <Project imageName="coding-quiz.gif" title="Coding Quiz" link="https://coding-quiz-6lep.onrender.com" githubRepo="https://github.com/selmgar/coding-quiz" />
         <Project imageName="weatherdashboard.gif" title="Weather Dash" link="https://weather-dashboard-zhx7.onrender.com/" githubRepo="https://github.com/selmgar/Weather-Dashboard" />
         <Project title="Project 3" link="" githubRepo="" />
         <Project title="Project 4" link="" githubRepo="" />
